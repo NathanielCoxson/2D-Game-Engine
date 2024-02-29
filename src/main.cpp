ab3a2abd72a925daf6b18f0e431227ed8f02230b
Nathan Coxson <nathancoxson2739@gmail.com>
2024-02-29 18:21:18 -0500

    removed unused test files

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,13 @@
 #include <fstream>
 
 int main() {
-	const int wWidth = 640;
-	const int wHeight = 480;
-	sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Game");
-	sf::CircleShape circle(50);
-	circle.setFillColor(sf::Color::Green);
-	circle.setPosition(300, 300);
-
+	//const int wWidth = 640;
+	//const int wHeight = 480;
+	//sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Game");
+	//sf::CircleShape circle(50);
+	//circle.setFillColor(sf::Color::Green);
+	//circle.setPosition(300, 300);
+    
 	sf::Font myFont;
 
 	if (!myFont.loadFromFile("bin/Roboto-Regular.ttf")) {
@@ -18,10 +18,20 @@ int main() {
 	}
 
 	std::ifstream infile;
-	infile.open("input.txt");
-	int input;
-	infile >> input;
-	std::cout << input << "\n";
+	infile.open("bin/config.txt");
+
+    int wWidth, wHeight;
+    if (infile.is_open()) {
+        infile >> wWidth >> wHeight;
+    } else {
+        std::cout << "File failed to open\n";
+        exit(1);
+    }
+    sf::RenderWindow window(sf::VideoMode(), "SFML Game");
+    sf::CircleShape circle(50);
+    circle.setFillColor(sf::Color::Green);
+    circle.setPosition(300, 300);
+
 
 	sf::Event e;
 	while (window.isOpen()) {
