85f139b3f3dac7a8d7824b46d6da588a8707422a
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-01 06:18:01 -0500

    added text labels to shapes

--- makefile
+++ makefile
@@ -1,3 +1,6 @@
+buildrun:
+	make build
+	make run
 build:
 	g++ src/*.cpp -Isrc -std=c++17 -o bin/sfmlgame -lsfml-graphics -lsfml-window -lsfml-system -lsfml-audio
 run:
--- src/main.cpp
+++ src/main.cpp
@@ -4,42 +4,54 @@
 #include <fstream>
 
 int main() {
-	sf::Font myFont;
 
-	if (!myFont.loadFromFile("bin/Roboto-Regular.ttf")) {
-		std::cout << "Font not found\n";
-	}
 
 	std::ifstream infile;
 	infile.open("bin/config.txt");
     int         wWidth;
     int         wHeight;
+    int         fontSize;
+    int         fontRed, fontGreen, fontBlue;
     std::string objectType;
     std::string fontFileName;
+	sf::Font    myFont;
+    sf::Color   textColor;
     if (infile.is_open()) {
         std::cout << "Config loaded successfully\n";
 
         // Load window dimensions and font information
         infile >> objectType >> wWidth >> wHeight;
-        infile >> objectType >> fontFileName;
+        infile >> objectType >> fontFileName >> fontSize;
+        infile >> fontRed >> fontGreen >> fontBlue;
+
+        if (myFont.loadFromFile(fontFileName)) {
+            std::cout << "Font loaded successfully\n";
+        } else {
+            std::cout << "Failed to load font file\n";
+            exit(1);
+        }
+
+        textColor = sf::Color(fontRed, fontGreen, fontBlue, 255);
+
     } else {
-        std::cout << "File failed to open\n";
+        std::cout << "Config file failed to open\n";
         exit(1);
     }
 
     // Read shapes
     std::vector<std::tuple<sf::CircleShape, float, float>>    circles;
     std::vector<std::tuple<sf::RectangleShape, float, float>> rectangles;
+    std::vector<std::tuple<sf::Text, float, float>>           labels;
     int         x, y;
     int         red, green, blue;
     int         radius;
     int         width, height;
     float       dx, dy;
-    std::string color;
+    std::string name;
     while (infile) {
         infile >> objectType;
         if (objectType.compare("Circle") == 0) {
-            infile >> color >> x >> y >> dx >> dy;
+            infile >> name >> x >> y >> dx >> dy;
             infile >> red >> green >> blue;
             infile >> radius;
 
@@ -48,8 +60,17 @@ int main() {
             circle.setFillColor(color);
             circle.setPosition(x, y);
             circles.push_back(std::make_tuple(circle, dx, dy));
+
+            sf::Text label(name, myFont);
+            label.setFillColor(textColor);
+            label.setCharacterSize(fontSize);
+            sf::FloatRect textRect = label.getLocalBounds();
+            label.setOrigin(textRect.left + textRect.width/2.0f,
+                            textRect.top + textRect.height/2.0f);
+            label.setPosition(x + radius, y + radius);
+            labels.push_back(std::make_tuple(label, dx, dy));
         } else if (objectType.compare("Rectangle") == 0) {
-            infile >> color >> x >> y >> dx >> dy;
+            infile >> name >> x >> y >> dx >> dy;
             infile >> red >> green >> blue;
             infile >> width >> height;
 
@@ -58,11 +79,19 @@ int main() {
             rectangle.setFillColor(color);
             rectangle.setPosition(x, y);
             rectangles.push_back(std::make_tuple(rectangle, dx, dy));
+
+            sf::Text label(name, myFont);
+            label.setFillColor(textColor);
+            label.setCharacterSize(fontSize);
+            sf::FloatRect textRect = label.getLocalBounds();
+            label.setOrigin(textRect.left + textRect.width/2.0f,
+                            textRect.top + textRect.height/2.0f);
+            label.setPosition(x + width/2.0f, y + height/2.0f);
+            labels.push_back(std::make_tuple(label, dx, dy));
         }
     }
     
     sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Game");
-
 	sf::Event e;
 	while (window.isOpen()) {
 		while (window.pollEvent(e)) {
@@ -71,8 +100,10 @@ int main() {
 			}
 		}
 
+        // Clear previous frame
 		window.clear(sf::Color::Black);
 
+        // Drawing
         for (auto &c: circles) {
             std::get<0>(c).move(std::get<1>(c), std::get<2>(c));
             window.draw(std::get<0>(c));
@@ -81,7 +112,12 @@ int main() {
             std::get<0>(r).move(std::get<1>(r), std::get<2>(r));
             window.draw(std::get<0>(r));
         }
+        for (auto &l: labels) {
+            std::get<0>(l).move(std::get<1>(l), std::get<2>(l));
+            window.draw(std::get<0>(l));
+        }
 
+        // Display new frame
 		window.display();
 	}
 
