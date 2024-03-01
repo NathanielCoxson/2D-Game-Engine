a0412ea2edbcb2abd344cc787fb4f340efb6d553
Nathan Coxson <nathancoxson2739@gmail.com>
2024-02-29 20:18:22 -0500

    added logic to read in shapes and then draw and move them on the screen

--- src/main.cpp
+++ src/main.cpp
@@ -4,13 +4,6 @@
 #include <fstream>
 
 int main() {
-	//const int wWidth = 640;
-	//const int wHeight = 480;
-	//sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Game");
-	//sf::CircleShape circle(50);
-	//circle.setFillColor(sf::Color::Green);
-	//circle.setPosition(300, 300);
-    
 	sf::Font myFont;
 
 	if (!myFont.loadFromFile("bin/Roboto-Regular.ttf")) {
@@ -19,19 +12,56 @@ int main() {
 
 	std::ifstream infile;
 	infile.open("bin/config.txt");
-
-    int wWidth, wHeight;
+    int         wWidth;
+    int         wHeight;
+    std::string objectType;
+    std::string fontFileName;
     if (infile.is_open()) {
-        infile >> wWidth >> wHeight;
+        std::cout << "Config loaded successfully\n";
+
+        // Load window dimensions and font information
+        infile >> objectType >> wWidth >> wHeight;
+        infile >> objectType >> fontFileName;
     } else {
         std::cout << "File failed to open\n";
         exit(1);
     }
-    sf::RenderWindow window(sf::VideoMode(), "SFML Game");
-    sf::CircleShape circle(50);
-    circle.setFillColor(sf::Color::Green);
-    circle.setPosition(300, 300);
 
+    // Read shapes
+    std::vector<std::tuple<sf::CircleShape, float, float>>    circles;
+    std::vector<std::tuple<sf::RectangleShape, float, float>> rectangles;
+    int         x, y;
+    int         red, green, blue;
+    int         radius;
+    int         width, height;
+    float       dx, dy;
+    std::string color;
+    while (infile) {
+        infile >> objectType;
+        if (objectType.compare("Circle") == 0) {
+            infile >> color >> x >> y >> dx >> dy;
+            infile >> red >> green >> blue;
+            infile >> radius;
+
+            sf::CircleShape circle(radius);
+            sf::Color color(red, green, blue, 255);
+            circle.setFillColor(color);
+            circle.setPosition(x, y);
+            circles.push_back(std::make_tuple(circle, dx, dy));
+        } else if (objectType.compare("Rectangle") == 0) {
+            infile >> color >> x >> y >> dx >> dy;
+            infile >> red >> green >> blue;
+            infile >> width >> height;
+
+            sf::RectangleShape rectangle(sf::Vector2f(width, height));
+            sf::Color          color(red, green, blue, 255);
+            rectangle.setFillColor(color);
+            rectangle.setPosition(x, y);
+            rectangles.push_back(std::make_tuple(rectangle, dx, dy));
+        }
+    }
+    
+    sf::RenderWindow window(sf::VideoMode(wWidth, wHeight), "SFML Game");
 
 	sf::Event e;
 	while (window.isOpen()) {
@@ -43,7 +73,14 @@ int main() {
 
 		window.clear(sf::Color::Black);
 
-		window.draw(circle);
+        for (auto &c: circles) {
+            std::get<0>(c).move(std::get<1>(c), std::get<2>(c));
+            window.draw(std::get<0>(c));
+        }
+        for (auto &r: rectangles) {
+            std::get<0>(r).move(std::get<1>(r), std::get<2>(r));
+            window.draw(std::get<0>(r));
+        }
 
 		window.display();
 	}
