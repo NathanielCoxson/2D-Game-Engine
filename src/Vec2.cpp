35c30a7fc02f6700e33e7d1ef6874f752f91e38a
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-17 19:27:12 -0400

    feat: added shorthand operators to Vec2 class

--- src/Vec2.cpp
+++ src/Vec2.cpp
@@ -19,6 +19,26 @@ Vec2 Vec2::operator / (const Vec2& rhs) const {
     return Vec2(x / rhs.x, y / rhs.y);
 }
 
+void Vec2::operator += (const Vec2& rhs) {
+    x += rhs.x;
+    y += rhs.y;
+}
+
+void Vec2::operator -= (const Vec2& rhs) {
+    x -= rhs.x;
+    y -= rhs.y;
+}
+
+void Vec2::operator *= (const Vec2& rhs) {
+    x *= rhs.x;
+    y *= rhs.y;
+}
+
+void Vec2::operator /= (const Vec2& rhs) {
+    x /= rhs.x;
+    y /= rhs.y;
+}
+
 bool Vec2::operator == (const Vec2& rhs) const {
     return x == rhs.x && y == rhs.y;
 }
--- src/Vec2.hpp
+++ src/Vec2.hpp
@@ -14,6 +14,12 @@ public:
     Vec2 operator -  (const Vec2& rhs) const;
     Vec2 operator *  (const Vec2& rhs) const;
     Vec2 operator /  (const Vec2& rhs) const;
+
+    void operator += (const Vec2& rhs);
+    void operator -= (const Vec2& rhs);
+    void operator *= (const Vec2& rhs);
+    void operator /= (const Vec2& rhs);
+
     bool operator == (const Vec2& rhs) const;
     bool operator != (const Vec2& rhs) const;
 
