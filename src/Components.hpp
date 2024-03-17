e0da5c3b4f5aa2882879798104ac11677fb25a67
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-17 19:35:31 -0400

    feat: added the transform component class (#6)

--- src/Components.hpp
+++ src/Components.hpp
@@ -1,5 +1,7 @@
-#ifndef COMPONENTS_H
-#define COMPONENTS_H
+#pragma once
+
+#include "Vec2.hpp"
+#include <SFML/Graphics.hpp>
 
 class CScore {
 
@@ -8,4 +10,13 @@ public:
     CScore(int s): score(s) {};
 };
 
-#endif
+class CTransform {
+    
+public: 
+    Vec2  pos      = { 0.0, 0.0 };
+    Vec2  velocity = { 0.0, 0.0 };
+    float angle    = 0;
+
+    CTransform(const Vec2& p, const Vec2& v, float a)
+        : pos(p), velocity(v), angle(a) {}
+};
