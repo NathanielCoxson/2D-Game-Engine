bc2dad59321057aecc599001c973103bf91f5183
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-19 04:47:51 -0400

    feat: added CShape component class (#11)

--- src/Components.hpp
+++ src/Components.hpp
@@ -20,3 +20,22 @@ public:
     CTransform(const Vec2& p, const Vec2& v, float a)
         : pos(p), velocity(v), angle(a) {}
 };
+
+class CShape {
+
+public:
+    sf::CircleShape circle;
+
+    CShape( float            radius, 
+            int              points, 
+            const sf::Color& fill,
+            const sf::Color& outline, 
+            float            thickness) 
+        : circle(radius, points)
+    {
+        circle.setFillColor(fill);
+        circle.setOutlineColor(outline);
+        circle.setOutlineThickness(thickness);
+        circle.setOrigin(radius, radius);
+    }
+};
