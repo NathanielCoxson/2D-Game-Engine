bf4fc9998de5063530cb7fe2bf55571fd3442f8a
Nathan Coxson <nathancoxson2739@gmail.com>
2024-03-19 05:17:05 -0400

    feat: added the Entity class

---
+++ src/Entity.cpp
@@ -0,0 +1,20 @@
+#include "Entity.hpp"
+
+Entity::Entity(const size_t id, const std::string& tag)
+    : m_id(id), m_tag(tag) {}
+
+bool Entity::isActive() const {
+    return m_active;
+}
+
+const std::string& Entity::tag() const {
+    return m_tag;
+}
+
+const size_t Entity::id() const {
+    return m_id;
+}
+
+void Entity::destory() {
+    m_active = false;
+}
---
+++ src/Entity.hpp
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "Components.hpp"
+#include <memory>
+
+class Entity {
+
+    friend class EntityManager;
+
+    bool        m_active = true;
+    size_t      m_id     = 0;
+    std::string m_tag    = "default";
+
+    Entity(const size_t id, const std::string& tag);
+
+public:
+    std::shared_ptr<CTransform> cTransform;
+    std::shared_ptr<CShape>     cShape;
+    std::shared_ptr<CCollision> cCollision;
+    std::shared_ptr<CInput>     cInput;
+    std::shared_ptr<CLifespan>  cLifespan;
+    std::shared_ptr<CScore>     cScore;
+
+    bool               isActive() const;
+    const std::string& tag() const;
+    const size_t       id() const;
+    void               destory();
+};
