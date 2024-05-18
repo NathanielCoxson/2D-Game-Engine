556d7bf19a1840b157b771c4f14e26dd16284825
Nathan Coxson <nathancoxson2739@gmail.com>
2024-05-18 19:17:20 -0400

    feat: added an animation class to hold animation data (#49)

---
+++ src/Animation.cpp
@@ -0,0 +1,43 @@
+#include "Animation.hpp"
+#include <cmath>
+
+Animation::Animation() {}
+
+Animation::Animation(const std::string& name, const sf::Texture& t)
+    : Animation(name, t, 1, 0) {}
+
+Animation::Animation(const std::string& name, const sf::Texture& t, size_t frameCount, size_t speed)
+    : m_name         (name)
+    , m_sprite       (t)
+    , m_frameCount   (frameCount)
+    , m_currentFrame (0)
+    , m_speed        (speed)
+{
+    m_size = Vec2((float)t.getSize().x / frameCount, (float)t.getSize().y);
+    m_sprite.setOrigin(m_size.x / 2.0f, m_size.y / 2.0f);
+    m_sprite.setTextureRect(sf::IntRect(std::floor(m_currentFrame) * m_size.x, 0, m_size.x, m_size.y));
+}
+
+void Animation::update() {
+    m_currentFrame++;
+    float animFrame = (m_currentFrame / m_speed) % m_frameCount;
+    sf::IntRect rectangle = sf::IntRect(std::floor(animFrame) * m_size.x, 0, m_size.x, m_size.y);
+    m_sprite.setTextureRect(rectangle);
+}
+
+bool Animation::hasEnded() const {
+    bool allFramesPlayed = (m_currentFrame / m_speed) >= m_frameCount;
+    return allFramesPlayed;
+}
+
+const std::string& Animation::getName() const {
+    return m_name;
+}
+
+const Vec2& Animation::getSize() const {
+    return m_size;
+}
+
+const sf::Sprite& Animation::getSprite() const {
+    return m_sprite;
+}
---
+++ src/Animation.hpp
@@ -0,0 +1,26 @@
+#pragma once
+
+#include <SFML/Graphics.hpp>
+#include "Vec2.hpp"
+#include <string>
+
+class Animation {
+    std::string m_name;
+    sf::Sprite  m_sprite;
+    Vec2        m_size = { 0.0, 0.0 };
+    size_t      m_frameCount;
+    size_t      m_currentFrame;
+    size_t      m_speed;
+
+public:
+    Animation(); 
+    Animation(const std::string& name, const sf::Texture& t);
+    Animation(const std::string& name, const sf::Texture& t, size_t frameCount, size_t speed);
+
+    void update();
+    bool hasEnded() const;
+
+    const std::string& getName()   const;
+    const Vec2&        getSize()   const;
+    const sf::Sprite&  getSprite() const;
+};
