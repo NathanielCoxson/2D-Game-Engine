d8352d597b8d86e544a88482c07c9e625d3d4d00
Nathan Coxson <nathancoxson2739@gmail.com>
2025-01-19 17:53:02 -0500

    added a function to entities to check if they can be killed based on their current state (#282)

--- src/Entity.cpp
+++ src/Entity.cpp
@@ -12,12 +12,23 @@ Entity::Entity(const size_t id, const std::string& tag)
         CAnimation(),
         CGravity(),
         CState(),
-        CCooldown()
+        CCooldown(),
+        CEffects()
     )
 {}
 
 bool Entity::isActive() const {
     return m_active;
+}
+
+bool Entity::canBeDestroyed() const {
+    CEffects effects = std::get<CEffects>(m_components);
+
+    bool isInvincible = effects.has and effects.isInvincible;
+
+    if (isInvincible) return false;
+
+    return true;
 }
 
 const std::string& Entity::tag() const {
